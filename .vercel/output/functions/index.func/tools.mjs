7c6d05beb7068421ce16b04e2fe36dd245e65201
Viktor1722 <viki1740@gmail.com>
2025-10-20 17:11:42 +0200

    Add Vercel output configuration files and initial function setup

---
+++ .vercel/output/functions/index.func/tools.mjs
@@ -0,0 +1,3 @@
+import * as tool0 from './tools/5b2831a0-f1c1-437c-a4a9-adf5cd9e6a90.mjs';
+
+export const tools = [tool0]
---
+++ .vercel/output/functions/index.func/tools/5b2831a0-f1c1-437c-a4a9-adf5cd9e6a90.mjs
@@ -0,0 +1,10 @@
+export { J as weatherTool } from '../mastra.mjs';
+import 'stream/web';
+import 'crypto';
+import 'fs';
+import 'module';
+import 'os';
+import 'path';
+import 'events';
+import 'pino';
+import 'pino-pretty';
